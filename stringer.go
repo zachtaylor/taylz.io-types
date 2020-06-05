1c6a86eac4456a03874c92d38d40fb195ecd8045
Zach Taylor <zach@ztaylor.me>
2020-06-05 16:13:54 -0500

    add Stringer wrappers
    add StringerDict
    add StringerMap
    add StringerSlice

--- stringer.go
+++ stringer.go
@@ -6,14 +6,23 @@ import "fmt"
 type Stringer = fmt.Stringer
 
 // NewStringer returns a wrapper Stringer that defers parsing the interal string
-func NewStringer(any I) Stringer {
-	return stringer{any}
-}
+func NewStringer(i I) Stringer { return stringer{i} }
 
-type stringer struct {
-	i I
-}
+type stringer struct{ I } // generic container
 
-func (i stringer) String() string {
-	return NewString(i.i)
-}
+func (stringer stringer) String() string { return NewString(stringer.I) }
+
+// StringerDict is a Stringer wrapper for Dict
+type StringerDict Dict
+
+func (dict StringerDict) String() string { return NewStringDict(dict) }
+
+// StringerMap is a Stringer wrapper for Map
+type StringerMap Map
+
+func (m StringerMap) String() string { return NewStringMap(m) }
+
+// StringerSlice is a Stringer wrapper for Slice
+type StringerSlice Slice
+
+func (slice StringerSlice) String() string { return NewStringSlice(slice) }
